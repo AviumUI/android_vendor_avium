f7838cf8a1deb3467e5b8c90f79bce3dae94e903
finish0314 <finish21190@gmail.com>
2025-08-25 16:16:36 +0800

    config: Add Avium version

--- config/avium.mk
+++ config/avium.mk
@@ -1,5 +1,9 @@
 # AviumUI configuration file
 
+# Build 
+AVIUM_BUILDTYPE ?= Unoffical
+AVIUM_VERSION_APPEND_TIME_OF_DAY ?= false
+
 # LatinIMEGooglePrebuilt
 TARGET_INCLUDE_GOOGLEIME ?= false
 TARGET_GOOGLEIME_OVERRIDE_IME ?= false 
@@ -7,3 +11,4 @@ TARGET_GOOGLEIME_OVERRIDE_IME ?= false
 # Include configs
 $(call inherit-product, vendor/avium/config/packages.mk)
 $(call inherit-product, vendor/avium/config/overlay.mk)
+$(call inherit-product, vendor/avium/config/version.mk)
---
+++ config/version.mk
@@ -0,0 +1,31 @@
+# Avium Version
+AVIUM_VER := AviumUI-$(PLATFORM_VERSION)-$(LINEAGE_BUILD)
+
+# Date format
+ifeq ($(AVIUM_VERSION_APPEND_TIME_OF_DAY),true)
+    AVIUM_BUILD_DATE := $(shell date -u +%Y%m%d_%H%M%S)
+else
+    AVIUM_BUILD_DATE := $(shell date -u +%Y%m%d)
+endif
+
+# Display version
+AVIUM_DISPLAY_VERSION := $(AVIUM_VER)
+
+# Because some devices have 'mtdoops.fingerprint' in cmdline.
+# Some device tree use LINEAGE_VERSION as default value.
+AVIUM_VERSION := $(AVIUM_VER)-$(AVIUM_BUILD_DATE)-$(AVIUM_BUILDTYPE)
+LINEAGE_VERSION := $(AVIUM_VERSION)
+
+# Package name
+ifeq ($(AVIUM_BUILDTYPE),Official)
+    AVIUM_PACKAGE_NAME := AviumUI-$(PLATFORM_VERSION)-$(LINEAGE_BUILD)-$(AVIUM_BUILD_DATE)-Official
+else
+    AVIUM_PACKAGE_NAME := AviumUI-$(PLATFORM_VERSION)-$(LINEAGE_BUILD)-$(AVIUM_BUILD_DATE)-Unofficial
+endif
+
+# AviumUI version properties
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.avium.version=$(AVIUM_VER) \
+    ro.avium.display.version=$(AVIUM_DISPLAY_VERSION) \
+    ro.avium.build.version=$(PLATFORM_VERSION) \
+    ro.avium.releasetype=$(AVIUM_BUILDTYPE)

c1e904850bd2ddd8b2515ee0bd5d8054f6e55590
finish0314 <finish21190@gmail.com>
2025-09-28 12:54:42 +0800

    envsetup: Add gen_keys

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -97,6 +97,27 @@ function avium_build() {
     fi
     mka $avium_target -j$(nproc --all)
 }
+
+function gen_keys() {
+    if ! [ -n "$TOP" ];then
+        echo "Couldn't locate the top of the tree.  Try setting TOP."
+        return 1
+        exit 1
+    fi
+    local subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
+    mkdir ~/.avium-certs
+    for cert in bluetooth cyngn-app media networkstack nfc platform releasekey sdk_sandbox shared testcert testkey verity; do
+    ./development/tools/make_key ~/.avium-certs/$cert "$subject"; \
+    done
+    cp ./development/tools/make_key ~/.avium-certs/
+    sed -i 's|2048|4096|g' ~/.avium-certs/make_key
+    for apex in com.android.adbd com.android.adservices com.android.adservices.api com.android.appsearch com.android.appsearch.apk com.android.art com.android.bluetooth com.android.btservices com.android.cellbroadcast com.android.compos com.android.configinfrastructure com.android.connectivity.resources com.android.conscrypt com.android.devicelock com.android.extservices com.android.graphics.pdf com.android.hardware.authsecret com.android.hardware.biometrics.face.virtual com.android.hardware.biometrics.fingerprint.virtual com.android.hardware.boot com.android.hardware.cas com.android.hardware.neuralnetworks com.android.hardware.rebootescrow com.android.hardware.wifi com.android.healthfitness com.android.hotspot2.osulogin com.android.i18n com.android.ipsec com.android.media com.android.media.swcodec com.android.mediaprovider com.android.nearby.halfsheet com.android.networkstack.tethering com.android.neuralnetworks com.android.nfcservices com.android.ondevicepersonalization com.android.os.statsd com.android.permission com.android.profiling com.android.resolv com.android.rkpd com.android.runtime com.android.safetycenter.resources com.android.scheduling com.android.sdkext com.android.support.apexer com.android.telephony com.android.telephonymodules com.android.tethering com.android.tzdata com.android.uwb com.android.uwb.resources com.android.virt com.android.vndk.current com.android.vndk.current.on_vendor com.android.wifi com.android.wifi.dialog com.android.wifi.resources com.google.pixel.camera.hal com.google.pixel.vibrator.hal com.qorvo.uwb; do \
+    subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN='$apex'/emailAddress=android@android.com'
+    ~/.avium-certs/make_key ~/.avium-certs/$apex "$subject"
+    openssl pkcs8 -in ~/.avium-certs/$apex.pk8 -inform DER -nocrypt -out ~/.avium-certs/$apex.pem
+    done
+}
+
 function avium() {
     local T=$(gettop)
     if [ -z "$T" ]; then

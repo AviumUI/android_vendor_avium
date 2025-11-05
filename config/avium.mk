11233fcfc9c0c062ea46d740ad4c41a449c6ca5e
finish0314 <finish21190@gmail.com>
2025-11-06 00:26:00 +0800

    config: Add flags to Set build feature

--- config/avium.mk
+++ config/avium.mk
@@ -19,12 +19,18 @@ PRODUCT_SYSTEM_PROPERTIES += ro.avium.maintainer=$(AVIUM_MAINTAINER)
 TARGET_INCLUDE_GOOGLEIME ?= false
 TARGET_GOOGLEIME_OVERRIDE_IME ?= false
 
+# Spoof Props
+# Set to true to enable spoofing fake props.
+# For letting apps think they are running on a locked device.
+AVIUM_FORCE_SET_FAKE_PROP ?= false
+
 # Include configs
 $(call inherit-product, vendor/avium/config/packages.mk)
 $(call inherit-product, vendor/avium/config/overlay.mk)
 $(call inherit-product, vendor/avium/config/version.mk)
 $(call inherit-product, vendor/avium/config/sepolicy.mk)
 $(call inherit-product, vendor/avium/config/gms.mk)
+$(call inherit-product, vendor/avium/config/soong_namespace.mk)
 
 # Enable Material Design 3 Expressive
 PRODUCT_PRODUCT_PROPERTIES += \

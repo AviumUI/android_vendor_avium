8a41bb24b6cd06f1819c7919fee5f22cd31f4f75
ruyue <ruyueee@outlook.com>
2025-11-20 19:06:03 +0800

    docs: Add copyright notice and license information

--- config/version.mk
+++ config/version.mk
@@ -1,3 +1,19 @@
+# 
+# Copyright (C) 2025 The AviumUI Project
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Avium Version
 AVIUM_VER := AviumUI-$(PLATFORM_VERSION)-$(LINEAGE_BUILD)
 
@@ -46,4 +62,4 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.avium.gms_status=$(WITH_GMS) \
 	ro.avium.maintainer=$(AVIUM_MAINTAINER) \
 	ro.avium.is_offical=$(AVIUM_IS_OFFICIAL) \
-    ro.avium.version=$(AVIUM_VER) 
+    ro.avium.version=$(AVIUM_VER)

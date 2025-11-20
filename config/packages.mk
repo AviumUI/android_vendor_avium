7fa2f67d46634a3634d0346540347eb0400de1a4
ruyue <ruyueee@outlook.com>
2025-11-20 18:40:41 +0800

    feat(Lock Screen): Add Support for Depth Wallpaper Feature

--- config/packages.mk
+++ config/packages.mk
@@ -26,7 +26,8 @@ PRODUCT_PACKAGES += \
     BtHelper \
     OmniJaws \
     Note \
-    PocketMode
+    PocketMode \
+    DepthWallpaperHelper
 
 #DepthWallpaperHelper
 
@@ -40,3 +41,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
      vendor/avium/prebuilt/common/etc/init/init.avium-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.avium-updater.rc
 endif
+
+# DepthWallpaperHelper
+PRODUCT_COPY_FILES += \
+    vendor/avium/prebuilt/media/wallpaper/mask:$(TARGET_COPY_OUT_SYSTEM_EXT)/media/wallpaper/mask \
+    vendor/avium/prebuilt/media/wallpaper/wallpaper:$(TARGET_COPY_OUT_SYSTEM_EXT)/media/wallpaper/wallpaper
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += persist.avium.depthwallpaper=0

a46d538aec92b659a7f3649a3a2b12cc7350c3cd
uwu-gl <moegluwu0@gmail.com>
2026-01-19 13:24:28 +0800

    avium: Clean blur & m3e configurations
    ro.sf.blurs_are_expensive is not existed at all;
    is_expressive_design_enabled is enabled by default on QPR1+.
    
    Ref: https://cs.android.com/android/platform/superproject/+/android-latest-release:build/release/aconfig/bp3a/com.android.settingslib.widget.theme.flags/is_expressive_design_enabled_flag_values.textproto?q=is_expressive_design_enabled&ss=android
    
    Change-Id: I43fd15a6a49884c0a809ad8b28f223f926478df2
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- config/prop.mk
+++ config/prop.mk
@@ -14,11 +14,7 @@
 # limitations under the License.
 #
 
-# Enable Material Design 3 Expressive
-PRODUCT_PRODUCT_PROPERTIES += is_expressive_design_enabled=true
-
 # Enable blur if maintainer asked
 ifeq ($(TARGET_ENABLE_BLUR), true)
 PRODUCT_PRODUCT_PROPERTIES += ro.surface_flinger.supports_background_blur=1
-PRODUCT_PRODUCT_PROPERTIES += ro.sf.blurs_are_expensive=true
 endif # TARGET_ENABLE_BLUR

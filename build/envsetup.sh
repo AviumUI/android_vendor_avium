78ea7e4820d9d848596116a1bb5b0d4f9f78dbbf
Modificator <yunshangcn@gmail.com>
2026-03-13 10:28:03 +0800

    fixup: stop using ls to iterate split part files
    Using `$(ls ...)` breaks when `ls` output contains ANSI color codes
    and is also unsafe for shell word-splitting. Switch to glob-based
    iteration and quote variable expansions.
    
    Change-Id: Icb8f981e7d09eeb4bef921d9f12d48f66074a529

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -143,7 +143,9 @@ function merge_file_parts() {
     part_prefix="$target_file"
     found_part=0
 
-    for part in $(ls "${part_prefix}".*.part 2>/dev/null); do
+    for part in "${part_prefix}".*.part; do
+        [ -e "$part" ] || continue
+
         if [ "$found_part" -eq 0 ]; then
             : > "$target_file"
             found_part=1
